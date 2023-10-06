c060af9c78c5c715e8a94f7cde27a6d46fd2d466
thiagoandradewp2 <contato@thiagoandrade.dev.br>
2023-10-06 20:34:18 -0300

    teste

--- functions.php
+++ functions.php
@@ -20,43 +20,15 @@ if ( ! defined( '_S_VERSION' ) ) {
  * as indicating support for post thumbnails.
  */
 function thiagoandradewp_setup() {
-	/*
-		* Make theme available for translation.
-		* Translations can be filed in the /languages/ directory.
-		* If you're building a theme based on Thiago Andrade, use a find and replace
-		* to change 'thiagoandradewp' to the name of your theme in all the template files.
-		*/
 	load_theme_textdomain( 'thiagoandradewp', get_template_directory() . '/languages' );
-
-	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
-
-	/*
-		* Let WordPress manage the document title.
-		* By adding theme support, we declare that this theme does not use a
-		* hard-coded <title> tag in the document head, and expect WordPress to
-		* provide it for us.
-		*/
 	add_theme_support( 'title-tag' );
-
-	/*
-		* Enable support for Post Thumbnails on posts and pages.
-		*
-		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		*/
 	add_theme_support( 'post-thumbnails' );
-
-	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
 			'menu-1' => esc_html__( 'Primary', 'thiagoandradewp' ),
 		)
 	);
-
-	/*
-		* Switch default core markup for search form, comment form, and comments
-		* to output valid HTML5.
-		*/
 	add_theme_support(
 		'html5',
 		array(
@@ -140,9 +112,7 @@ add_action( 'widgets_init', 'thiagoandradewp_widgets_init' );
 function thiagoandradewp_scripts() {
 	wp_enqueue_style( 'thiagoandradewp-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'thiagoandradewp-style', 'rtl', 'replace' );
-
 	wp_enqueue_script( 'thiagoandradewp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
