eac6bc5c8086f1f059d987cd6f7cc0ec73c79370
thiagoandradewp2 <contato@thiagoandrade.dev.br>
2023-10-12 17:28:14 -0300

    ajustando page

--- page.php
+++ page.php
@@ -19,14 +19,10 @@ get_header();
 
 		<?php
 		while ( have_posts() ) :
-			the_post();
+			
+			the_title( );
+			the_content() ;
 
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
 
 		endwhile; // End of the loop.
 		?>
