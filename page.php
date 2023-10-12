24f3e271ea1810a29a4c116ab75fe5eaf5495589
thiagoandradewp2 <contato@thiagoandrade.dev.br>
2023-10-12 17:21:16 -0300

    alterando page

--- page.php
+++ page.php
@@ -15,7 +15,7 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main class="main-page">
 
 		<?php
 		while ( have_posts() ) :
@@ -31,8 +31,9 @@ get_header();
 		endwhile; // End of the loop.
 		?>
 
-	</main><!-- #main -->
+
+
+	</main>
 
 <?php
-get_sidebar();
 get_footer();
